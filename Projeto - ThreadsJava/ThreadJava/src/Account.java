a2110dc28685faa3852a87cb2e06c049d492e67f
SocioPJ <20.00704-3@maua.br>
2022-05-27 15:18:15 -0300

    algumas alterações

--- Projeto - ThreadsJava/ThreadJava/src/Account.java
+++ Projeto - ThreadsJava/ThreadJava/src/Account.java
@@ -1,17 +1,37 @@
-package prodcon;
 import java.util.Random;
 
 public class Account{
-    private double balance;
+    Random random = new Random();
+    private int balance;
     private double initialBalance;
     public Account(double initialBalance) {
         this.initialBalance = initialBalance;
     }
     public void deposit(){
-
+        Random random = new Random();
+		try {
+			// Tenta produzir um número inteiro aleatório
+			while (true) {
+				balance.put(random.nextInt(1000));
+				// Dorme 200 ms
+				Thread.sleep(200);
+				// Ou comente a linha anterior e
+				// descomente a linha de baixo
+				// e dê a chance para outra thread
+				// Thread.yield();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
     }
 
     public void withdraw(){
 
+    }
+    public int getBalance() {
+        return balance;
+    }
+    public void setBalance(int balance) {
+        this.balance = balance;
     }
 }
